925e072bd71b0b047d1e64dc823bb9c1e77fb468
ivan <iizdepskyy112@gmail.com>
2019-09-22 10:29:04 +0300

    Filmography Part 1

---
+++ src/main/java/org/izdebski/controller/FilmController.java
@@ -0,0 +1,36 @@
+package org.izdebski.controller;
+
+import org.izdebski.model.Film;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FilmController {
+
+    private static Film film;
+
+    static {
+        film = new Film();
+        film.setTitle("Inception");
+        film.setYear(2010);
+        film.setGenre("sci-fi");
+        film.setWatched(true);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView allFilms() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("films");
+        modelAndView.addObject("film", film);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public ModelAndView editPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        return modelAndView;
+    }
+}
